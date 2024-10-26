eb0a613d7c9eaba8f7aed37eaf19764a860a6a63
phamhieu <phamtronghieu2002iuh@gmail.com>
2024-10-26 16:06:29 +0700

    add google service

--- App/App.js
+++ App/App.js
@@ -12,69 +12,29 @@ import I18nProvider from './src/providers/I18nProvider';
 
 import { useState, useEffect, useRef } from 'react';
 import { Text, View, Button, Platform } from 'react-native';
-import * as Device from 'expo-device';
-import * as Notifications from 'expo-notifications';
+
 import { getData } from './src/utils/asyncStore';
 
-Notifications.setNotificationHandler({
-  handleNotification: async () => ({
-    shouldShowAlert: true,
-    shouldPlaySound: false,
-    shouldSetBadge: false,
-  }),
-});
 
 
-// const App = () => {
+import 'firebase/messaging';
+import firebase from 'firebase/app';
 
-//   return (
-//     // nguyen thanh doanh
-//     //trong hieu
-//     //thanhdoanh
-//     //test 2
-
-
-
-
-
-
-//   );
-// };
-
-// export default App;
 
 
 export default function App() {
-  const [expoPushToken, setExpoPushToken] = useState('');
-  const [notification, setNotification] = useState(false);
-  const notificationListener = useRef();
-  const responseListener = useRef();
 
-  useEffect(() => {
-    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
-
-    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
-      setNotification(notification);
-    });
-
-    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
-      console.log(response);
-    });
-
-    return () => {
-      Notifications.removeNotificationSubscription(notificationListener.current);
-      Notifications.removeNotificationSubscription(responseListener.current);
-    };
-  }, []);
-
-  useEffect(() => {
-    const run  = async () => {
-      await scheduleDailyPushNotification();
-    }
-    // run();
-   
-  }
-    , []);
+  
+  // if (!firebase?.apps?.length) {
+  //   firebase.initializeApp({
+  //     apiKey: "AIzaSyB1KVGw-193zuTNO9i3NrgysoeWOIaiLpE",
+  //     projectId: "ieltsapp-a4cfa",
+  //     storageBucket: "ieltsapp-a4cfa.appspot.com",
+  //     messagingSenderId: "1075445541957",
+  //     appId: "1:1075445541957:android:73561303a58c595a26872c",
+  //   });
+  // }
+ 
   return (
 
         <Provider store={store}>
@@ -91,67 +51,5 @@ export default function App() {
     </Provider>
 
   );
-}
-
-async function scheduleDailyPushNotification() {
-  const now = new Date();
-  const hours = 22;  // 8:00 PM
-  const minutes = 54;
-
-  // Tính thời gian đến 8:00 PM hôm nay hoặc ngày mai nếu hiện tại đã qua 8:00 PM
-  let triggerDate = new Date(now);
-  triggerDate.setHours(hours);
-  triggerDate.setMinutes(minutes);
-  triggerDate.setSeconds(0);
-
-  if (triggerDate < now) {
-    // Nếu đã qua 8:00 PM, đặt lịch cho ngày mai
-    triggerDate.setDate(triggerDate.getDate() + 1);
-  }
-
-  await Notifications.scheduleNotificationAsync({
-    content: {
-      title: "Time to check!",
-      body: "This is your daily 8:00 PM notification",
-      data: { data: 'goes here' },
-    },
-    trigger: {
-      hour: hours,
-      minute: minutes,
-      repeats: true,  // Để lặp lại hằng ngày
-    },
-  });
-}
-
-async function registerForPushNotificationsAsync() {
-  let token;
-
-  if (Platform.OS === 'android') {
-    await Notifications.setNotificationChannelAsync('default', {
-      name: 'default',
-      importance: Notifications.AndroidImportance.MAX,
-      vibrationPattern: [0, 250, 250, 250],
-      lightColor: '#FF231F7C',
-    });
-  }
-
-  if (Device.isDevice) {
-    const { status: existingStatus } = await Notifications.getPermissionsAsync();
-    let finalStatus = existingStatus;
-    if (existingStatus !== 'granted') {
-      const { status } = await Notifications.requestPermissionsAsync();
-      finalStatus = status;
-    }
-    if (finalStatus !== 'granted') {
-      alert('Failed to get push token for push notification!');
-      return;
-    }
-    token = (await Notifications.getExpoPushTokenAsync({ projectId: 'your-project-id' })).data;
-    console.log(token);
-  } else {
-    alert('Must use physical device for Push Notifications');
-  }
-
-  return token;
 }
 
--- App/src/components/Notify/Notify.js
+++ App/src/components/Notify/Notify.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types'
 import {
     View,
@@ -11,31 +11,174 @@ import {
     TextInput,
     TouchableOpacity,
     Pressable,
+    Platform
 } from 'react-native';
 import { useState } from 'react';
 import { Switch } from 'react-native-switch';
-import DateTimePicker from '@react-native-community/datetimepicker';
+import { DateTimePickerAndroid } from '@react-native-community/datetimepicker';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
-const convertTimeToHHMM = (date) => {
+
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+
+
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+      shouldShowAlert: true,
+      shouldPlaySound: false,
+      shouldSetBadge: false,
+    }),
+  });
+
+export const convertTimeToHHMM = (date) => {
     const hours = date.getHours().toString().padStart(2, '0');
     const minutes = date.getMinutes().toString().padStart(2, '0');
     return `${hours}:${minutes}`;
 };
 
-const Notify = ({textStyle="text-lg", Icon, rounded, border = 'border', title = 'Notifycation', onHandle, backGround, className ,circleSize}) => {
+const Notify = ({ textStyle = "text-lg", Icon, rounded, border = 'border', title = 'Notifycation', onHandle, backGround, className, circleSize }) => {
     const [time, setTime] = useState(new Date());
     const [show, setShow] = useState(false);
     const [switchValue, setSwitchValue] = useState(false);
-    const onChange = (event, selectedTime) => {
-        console.log('selectedTime >>>', selectedTime);
+    
 
+    const notificationListener = useRef();
+    const responseListener = useRef();
+  
+
+    const [expoPushToken, setExpoPushToken] = useState('');
+    const [notification, setNotification] = useState(false);
+
+
+
+
+    useEffect(() => {
+        registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+    
+        notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+          setNotification(notification);
+        });
+    
+        responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+          console.log(response);
+        });
+    
+        return () => {
+          Notifications.removeNotificationSubscription(notificationListener.current);
+          Notifications.removeNotificationSubscription(responseListener.current);
+        };
+      }, []);
+    
+      useEffect(() => {
+        const run  = async () => {
+        try {
+            await scheduleDailyPushNotification();
+        } catch (error) {
+            console.log("error 3", error);
+            
+        }
+        }
+        run();
+       
+      }
+        , []);
+
+
+
+
+
+
+        async function scheduleDailyPushNotification() {
+            try {
+                const now = new Date();
+                const hours = 16;  
+                const minutes = 1;
+              
+             
+                let triggerDate = new Date(now);
+                triggerDate.setHours(hours);
+                triggerDate.setMinutes(minutes);
+                triggerDate.setSeconds(0);
+              
+                if (triggerDate < now) {
+                  // Nếu đã qua 8:00 PM, đặt lịch cho ngày mai
+                  triggerDate.setDate(triggerDate.getDate() + 1);
+                }
+              
+                await Notifications.scheduleNotificationAsync({
+                  content: {
+                    title: "Đến giờ học Ielts rồi vô học điiiii!",
+                    body: "Đây là thông báo thời gian học tập nha!!!",
+                    data: { data: 'lets do it' },
+                  },
+                  trigger: {
+                    hour: hours,
+                    minute: minutes,
+                    repeats: true,  // Để lặp lại hằng ngày
+                  },
+                });
+            } catch (error) {
+                console.log("error 2 >>", error);
+                
+            }
+          }
+          
+          async function registerForPushNotificationsAsync() {
+                try {
+                    let token;
+          
+                    if (Platform.OS === 'android') {
+                      await Notifications.setNotificationChannelAsync('default', {
+                        name: 'default',
+                        importance: Notifications.AndroidImportance.MAX,
+                        vibrationPattern: [0, 250, 250, 250],
+                        lightColor: '#FF231F7C',
+                      });
+                    }
+                  
+                    if (Device.isDevice) {
+                      const { status: existingStatus } = await Notifications.getPermissionsAsync();
+                      let finalStatus = existingStatus;
+                      if (existingStatus !== 'granted') {
+                        const { status } = await Notifications.requestPermissionsAsync();
+                        finalStatus = status;
+                      }
+                      if (finalStatus !== 'granted') {
+                        alert('Failed to get push token for push notification!');
+                        return;
+                      }
+                      token = (await Notifications.getExpoPushTokenAsync({ projectId: 'your-project-id' })).data;
+                      console.log(token);
+                    } else {
+                      alert('Must use physical device for Push Notifications');
+                    }
+                  
+                    return token;
+                } catch (error) {
+                    console.log("error 1", error);
+                    
+                }
+          }
+          
+          
+
+
+    const onChange = (event, selectedTime) => {
+        const timeConvert = convertTimeToHHMM(selectedTime);
+        setTime(selectedTime);
         const currentTime = selectedTime || time;
         setShow(Platform.OS === 'android');
         setTime(currentTime);
     };
 
     const showTimepicker = () => {
-        setShow(true);
+        DateTimePickerAndroid.open({
+            value: time,
+            onChange,
+            mode: 'time',
+            is24Hour: true,
+
+        });
     };
 
     return (
@@ -44,7 +187,7 @@ const Notify = ({textStyle="text-lg", Icon, rounded, border = 'border', title = 
             {Icon}
             <Text className={textStyle}>{title}</Text>
 
-            <View
+            <Pressable
                 onPress={showTimepicker}
 
                 className="shadow-md rounded-lg  w-[90px] h-[34px] border border-slate-300 hover:border-indigo-300flex flex-row items-center justify-center">
@@ -53,7 +196,7 @@ const Notify = ({textStyle="text-lg", Icon, rounded, border = 'border', title = 
                 </TouchableOpacity>
 
                 {<FontAwesome name="caret-down" size={20} color="#000" />}
-            </View>
+            </Pressable>
             {/* switch button here */}
             <Switch
                 backgroundActive={'#FFFF00'}
@@ -66,16 +209,8 @@ const Notify = ({textStyle="text-lg", Icon, rounded, border = 'border', title = 
                 }}
                 value={switchValue}
             />
-            {show && (
-                <DateTimePicker
-                    testID="dateTimePicker"
-                    value={time}
-                    mode="time"
-                    is24Hour={true}
-                    display="default"
-                    onChange={onChange}
-                />
-            )}
+
+
 
         </View>
     );
